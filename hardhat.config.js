f86c5f637d2741e5d281e915800cd37e51247752
Keshav Mundhra <mundhrakeshav11@gmail.com>
2021-06-28 09:47:45 +0530

    setup: add RPC

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,8 @@
 require("@nomiclabs/hardhat-waffle");
 require("solidity-coverage");
 require("hardhat-gas-reporter");
+require('dotenv').config();
+
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async () => {
@@ -10,9 +12,9 @@ task("accounts", "Prints the list of accounts", async () => {
     console.log(account.address);
   }
 });
-
+console.log(`${process.env.MAINNET_RPC}`);
 module.exports = {
-    solidity: {
+  solidity: {
     version: "0.8.0",
     settings: {
       optimizer: {
@@ -26,7 +28,11 @@ module.exports = {
     gasPrice: 21
   },
   networks: {
-    
+    hardhat: {
+      forking: {
+        url: `${process.env.MAINNET_RPC}`
+      }
+    }
   }
 };
 
